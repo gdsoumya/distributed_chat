1921ca52d98831736ae4c243242b6386668646c5
Paul Pham <148553+cryptogoth@users.noreply.github.com>
2020-05-13 16:55:16 -0500

    Preliminary React refactoring.

--- src/websockClient.js
+++ src/websockClient.js
@@ -23,10 +23,6 @@ const WebWebSocket = class {
     }
   }
 
-  send(data) {
-    return this.connection.send(data)
-  }
-
 }
 
 // Auto
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,19 +3,16 @@ import logo from './logo.svg';
 import './App.css';
 const { WebSocketClient } = require('darkchat');
 
-function App() {
+const wsc = new WebSocketClient({
+  host: 'capetown.arcology.nyc',
+  port: 8547,
+  useWSS: true,
+});
 
-  const [mode, changeMode] = useState('joinChannel');
-  const [channel, setChannel] = useState('');
-  const [username, setUsername] = useState('');
+const initWebsocket = (conn: any) => {
+  conn.start();
 
-  const wsc = new WebSocketClient({
-    host: 'capetown.arcology.nyc',
-    port: 8547,
-    useWSS: true,
-  });
-
-  wsc.on('close', (data: any) => {
+  conn.on('close', (data: any) => {
     console.error('disconnected', data);
     let li = document.createElement('li');
     li.innerText = "Disconnected From Server";
@@ -23,11 +20,11 @@ function App() {
     chat && chat.append(li);
   });
 
-  wsc.on('error', (error: any) => {
+  conn.on('error', (error: any) => {
     console.error('failed to connect', error);
   });
 
-  wsc.on('message', async (event : any) => {
+  conn.on('message', async (event : any) => {
     console.log('received', event.data);
     let li = document.createElement('li');
     // This is a Blob in the browser for some WebSocket messages
@@ -47,6 +44,56 @@ function App() {
       chat && chat.append(img);
     }
   });
+}
+
+// since they all have one source of truth
+const emit = (conn: any, payload: object) => {
+  console.log("Connection Object", conn, payload);
+  conn.connection.send(JSON.stringify(payload));
+}
+// too much abstraction ?
+const makeEmit = (type: string) => (conn: any, params: object) => {
+  return emit(conn, { type, ...params });
+}
+const sendMessage = makeEmit('msg');
+const joinChannel = makeEmit('join');
+
+const openConnection = (conn: any, callback: Function) => conn.on('open', callback);
+
+function App() {
+  const [mode, changeMode] = useState('joinChannel');
+  const [channel, setChannel] = useState('');
+  const [username, setUsername] = useState('');
+  const [message, setMessage] = useState('');
+
+  const sendMessageTo = (uname: string) => () => {
+    sendMessage(wsc, { uname, msg: message })
+  }
+
+  const createConnection = React.useCallback((evt: any) => {
+    evt.preventDefault(); // prevents the form from refreshing the page
+    console.info('Opening WS Connection');
+    initWebsocket(wsc)
+    openConnection(wsc, () => {
+      console.log('connected');
+      // change chat mode when connection is successful
+      changeMode('chat');
+      joinChannel(wsc, { uname: username, cname: channel });
+      let li = document.createElement('li');
+      li.innerText = `Connected to ${wsc.url}`;
+      const chat = document.querySelector('#chat')
+      chat && chat.append(li);
+    });
+  }, [channel, username]);
+
+
+  React.useEffect(() => {
+    console.table({
+      mode,
+      channel,
+      username
+    });
+  }) 
 
   return (
     <div className="App">
@@ -58,53 +105,38 @@ function App() {
         </p>
 
         { (mode === 'joinChannel') ? (
-          <form id="start">
+          <form id="start" onSubmit={createConnection}>
             <input
               type="text"
               id="uname"
               placeholder="USERNAME"
-              onChange={(evt) => setUsername(evt.target.value)}
+              onChange={(evt) => {
+                setUsername(evt.target.value)
+              }}
             />
             <input
               type="text"
               id="cname"
               placeholder="CHANNEL NAME"
-              onChange={(evt) => setChannel(evt.target.value)}
-            />
-            <button
-              type="submit"
-              onClick={() => {
-                changeMode('chat');
-
-                wsc.on('open', () => {
-                  wsc.send(JSON.stringify({
-                    type: 'join', username: username, msg: channel,
-                  }));
-                  let li = document.createElement('li');
-                  li.innerText = `Connected to ${wsc.url}`;
-                  const chat = document.querySelector('#chat')
-                  chat && chat.append(li);
-
-                  console.log('connected');
-                });
-
-
+              onChange={(evt) => {
+                setChannel(evt.target.value)
               }}
-             >
-               Start Chat
-             </button>
+            />
+            <button type="submit">Start Chat</button>
           </form>
         ) : ( 
           <div>
             <ul id="chat"></ul>
-
-            <form id="input">
-              <input type="text" id="message"></input>
+            <form onSubmit={sendMessageTo(username)}>
+              <input 
+                type="text" 
+                value={message}
+                onChange={(evt: any) => {
+                  const { value } = evt.target;
+                  if (!!value) setMessage(value)
+                }} />
               <br/>
-              <button
-                type="submit"
-                onClick={() => {}}
-              >Send</button>
+              <button type="submit">Send</button>
             </form>
           </div>
         )
